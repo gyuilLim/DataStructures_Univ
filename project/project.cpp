7a997821efce1acca711e50e81c240e16dbfc35e
gyuilLim <edu9059@naver.com>
2020-05-22 11:08:21 +0900

    add

--- project/project.cpp
+++ project/project.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
 using namespace std;
 
+void printMap(int map[][7], int n) {
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
+            if (map[i][j] == 0){
+                cout << "■";
+            }
+            else{
+                cout << "□";
+            }
+        }
+        cout << "\n";
+    }
+}
+
 int main() {
-    
+    int map[7][7] = {{0,0,0,0,0,0,0},
+                 {0,0,0,0,0,0,0},
+                 {0,0,0,0,0,0,0},
+                 {0,0,0,0,0,0,0},
+                 {0,0,0,0,0,0,0},
+                 {0,0,0,0,0,0,0},
+                 {0,0,0,0,0,0,0}};
+
+    printMap(map, 7);
 
     return 0;
 }
